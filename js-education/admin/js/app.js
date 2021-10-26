73a9729096ec04c9358a624a5e985ef389e02a37
Bohdan Shymanskyi <bohdan_shymanskyi@epam.com>
2021-10-26 13:48:09 +0300

    updts

--- js-education/admin/js/app.js
+++ js-education/admin/js/app.js
@@ -1,17 +1,36 @@
-let container = document.getElementById('container')
-let submit = document.getElementById('submit')
-let table = document.createElement('table')
-let inputName = document.getElementById('username');
-let select = document.getElementById('workplace');
+const container = document.getElementById('container')
+const submit = document.getElementById('submit')
+const table = document.createElement('table')
+const inputName = document.getElementById('username');
+const select = document.getElementById('workplace');
+let activeItem;
 
 function setData(data = []) {
-  localStorage.setItem('allUsers', JSON.stringify(data))
+  try {
+    localStorage.setItem('allUsers', JSON.stringify(data))
+  } catch (e) {
+    alert(e.message)
+  }
 }
 function getData() {
-  return JSON.parse(localStorage.getItem('allUsers'))
+  try {
+    return JSON.parse(localStorage.getItem('allUsers'))
+  } catch (e) {
+    alert(e.message)
+  }
 }
 function addEntry() {
-  let dataOfUsers = getData();
+   let dataOfUsers = getData() || [];
+  if(activeItem) {
+    if (inputName.value === '' || select.value === 'Work Department') {
+    alert('Please fill all forms')
+  } else {
+    dataOfUsers[activeItem].userName = inputName.value;
+    dataOfUsers[activeItem].department = select.value;
+    dataOfUsers[activeItem].editDate = new Date().toUTCString();
+    activeItem = null;
+    }
+  } else {
   if (inputName.value === '' || select.value === 'Work Department') {
     alert('Please fill all forms')
   } else {
@@ -22,13 +41,14 @@ function addEntry() {
       'editDate': '-'
     };
     dataOfUsers.push(user);
-    setData(dataOfUsers)
-    setTable()
+    }
   }
+  setData(dataOfUsers)
+  setTable()
 };
 
 function setTable() {
-  let users = getData()
+  let users = getData() || []
 
   if (users.length == 0) {
     table.style.display = 'none'
@@ -44,7 +64,7 @@ function setTable() {
 
     for (let i = 0; i < users.length; i++) {
       let addRow = document.createElement('tr');
-      addRow.id = i;
+      addRow.setAttribute('data-id', i)
       addRow.innerHTML = `
       <td id = 'rowName'>${users[i].userName}</td>
     <td id = 'rowDepartment'>${users[i].department}</td>
@@ -60,35 +80,23 @@ function setTable() {
 
   let editItem = document.querySelectorAll('#edit')
   for (let edit of editItem) {
-    edit.onclick = (e) => {
-      let el = e.target.parentNode.parentNode.id
-      submit.addEventListener('click', editTable, { once: true })
-      function editTable() {
-        if (inputName.value === '' || select.value === 'Work Department') {
-          alert('Please fill all forms')
-        } else {
-          submit.removeEventListener('click', addEntry)
-          users[el].userName = inputName.value;
-          users[el].department = select.value;
-          users[el].editDate = new Date().toUTCString();
-          setData(users)
-          setTable()
-        }
+    edit.addEventListener('click', getEditItemId)
+    function getEditItemId(e){
+      activeItem = e.target.parentNode.parentNode.dataset.id
+      inputName.value = users[activeItem].userName
+      select.value = users[activeItem].department
       }
-    }
   }
-
   let deleteItem = document.querySelectorAll('#delete')
   deleteItem.forEach(item => item.onclick = (e) => {
     let areYouSure = confirm('Are you sure to delete this user info?')
     if (areYouSure) {
-      let el = e.target.parentNode.parentNode.id
+      let el = e.target.parentNode.parentNode.dataset.id
       users = users.filter(item => item !== users[el])
       setData(users)
       setTable()
     }
   })
 }
-
 submit.addEventListener('click', addEntry)
 setTable()
